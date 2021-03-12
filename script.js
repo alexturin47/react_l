67609efe6e5f673d45a5c54c6526b1f85528b943
Alex Tyurin <alexturin@gmail.com>
2021-03-12 16:54:23 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function button(){
+	return 'button';
+}
+
+class Slider {
+	constructor(width, height, count){
+		this.width = width;
+		this.height = height;
+		this.count = count;
+	}
+
+	nextSlide(){
+		console.log('Move forvard');
+	}
+
+	prevSlide(){
+		console.log('Move back');
+	}
+
+	whoAmI(){
+		console.log(this.width, this.height, this.count);
+	}
+}
+
+const slider = new Slider(600, 400, 5);
+
+slider.whoAmI();
+
